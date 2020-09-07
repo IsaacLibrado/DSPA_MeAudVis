7a3e4578c6790934542c6b252d9081b5eb16102c
Samuel Isaac Librado Almada <samuelisaaclibradoalmada@hotmail.com>
2020-09-07 14:38:23 -0500

    Creacion clase CUsuario

---
+++ DSPA_MeAudVis/DSPA_MeAudVis.Web/Data/Entities/CUsuario.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSPA_MeAudVis.Web.Data.Entities
+{
+    public class CUsuario : IdentityUser
+    {
+        public string Contrasena { set; get; }
+        public int Matricula { set; get; }
+        public string Nombre { set; get; }
+        public string Apellido { set; get; }
+        public string NombreCompleto { get { return $"{Nombre} {Apellido}"; } }
+        public string TipoUsuario { set; get; }
+        public ICollection<CManual> Manuales { set; get; }
+
+        public void InicioSesion()
+        {
+
+        }
+
+        public void CierreSesion()
+        {
+
+        }
+
+        public void ConsultaInformacion()
+        {
+
+        }
+
+        public void CambiarContrasena()
+        {
+
+        }
+
+        public void RevisionManuales()
+        {
+
+        }
+    }
+}
