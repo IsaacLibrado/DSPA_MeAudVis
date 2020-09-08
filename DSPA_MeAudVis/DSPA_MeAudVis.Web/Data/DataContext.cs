841cce81870f6d2688debc5af24eb6fe2df7d407
Oscar Villegas <oscar_arturo_villegas@hotmail.com>
2020-09-08 17:54:44 -0500

    Creacion DataContext

---
+++ DSPA_MeAudVis/DSPA_MeAudVis.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace DSPA_MeAudVis.Web.Data
+{
+    using DSPA_MeAudVis.Web.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+    public class DataContext : DbContext
+    {
+        DbSet<CUsuario> Usuarios { get; set; }
+        //DbSet<CAdministrador> Administradores { get; set; }
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+    }
+}
