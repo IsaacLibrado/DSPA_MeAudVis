20ac85745ba204216856466a65a09d6849042b5d
David <david.hdezalv29@gmail.com>
2020-09-08 19:04:37 -0500

    Creacion de la clase CPrestamos Creacion de la clase CMaterial Declaracion de variables

---
+++ DSPA_MeAudVis/DSPA_MeAudVis.Web/Data/Entities/CMaterial.cs
@@ -0,0 +1,18 @@
+﻿using DSPA_MeAudVis.Web.Controllers.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSPA_MeAudVis.Web.Data.Entities
+{
+    public class CMaterial : IEntity
+    {
+        public int Id { set; get; }
+        public string Nombre { set; get; }
+        public string Etiqueta { set; get; }
+        public string Marca { set; get; }
+        public string Modelo { set; get; }
+        public string NumSerie { set; get; } 
+    }
+}
---
+++ DSPA_MeAudVis/DSPA_MeAudVis.Web/Data/Entities/CPrestamo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DSPA_MeAudVis.Web.Controllers.Data.Entities;
+
+namespace DSPA_MeAudVis.Web.Data.Entities
+{
+    public class CPrestamo : IEntity
+    {
+        public int Id { set; get; }
+        public CMaterial material { set; get; }
+        public CBecario becarioSalida { set; get; }
+        public CBecario becarioEntrega { set; get; }
+        public CSolicitante solicitante { set; get; }
+        public DateTime FechaHoraSalida { set; get; }
+        public DateTime FechaHoraEntrega { set; get; }
+
+    }
+}
