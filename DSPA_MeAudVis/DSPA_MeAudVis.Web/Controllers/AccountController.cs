8e32ff2978db3ce0728de4b378f3694359dd6562
Samuel Isaac Librado Almada <samuelisaaclibradoalmada@hotmail.com>
2020-09-24 16:58:46 -0500

    Creacion inicial de modelos, vistas y controladores para el login

---
+++ DSPA_MeAudVis/DSPA_MeAudVis.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿
+namespace DSPA_MeAudVis.Web.Controllers
+{
+    using DSPA_MeAudVis.Web.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore.Metadata.Internal;
+    using System.Threading.Tasks;
+
+    //controlador para el inicio de sesion
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ DSPA_MeAudVis/DSPA_MeAudVis.Web/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DSPA_MeAudVis.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        [MinLength(4, ErrorMessage = "{0} must have minimun {1} charactes")]
+        [EmailAddress]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [MinLength(4, ErrorMessage = "{0} must have minimun {1} charactes")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
