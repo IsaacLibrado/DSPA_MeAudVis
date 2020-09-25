c11022a72a70a2f82763eead34fcd83863aa143f
Samuel Isaac Librado Almada <samuelisaaclibradoalmada@hotmail.com>
2020-09-25 10:27:07 -0500

    Creacion de helpers

--- DSPA_MeAudVis/DSPA_MeAudVis.Web/Controllers/AccountController.cs
+++ DSPA_MeAudVis/DSPA_MeAudVis.Web/Controllers/AccountController.cs
@@ -1,9 +1,9 @@
-﻿
-namespace DSPA_MeAudVis.Web.Controllers
+﻿namespace DSPA_MeAudVis.Web.Controllers
 {
     using DSPA_MeAudVis.Web.Models;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore.Metadata.Internal;
+    using System.Linq;
     using System.Threading.Tasks;
 
     //controlador para el inicio de sesion
@@ -12,14 +12,31 @@ namespace DSPA_MeAudVis.Web.Controllers
         public IActionResult Login()
         {
             if (User.Identity.IsAuthenticated)
+            {
                 return RedirectToAction("Index", "Home");
-            
+            }
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var result = await _userHelper.LoginAsync(model.Username, model.Password, model.RememberMe);
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(this.Request.Query["ReturnUrl"].First());
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Usuario o contraseña no válida.");
+            model.Password = string.Empty;
             return View(model);
         }
     }
